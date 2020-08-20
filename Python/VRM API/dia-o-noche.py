6e7ef4b15aed4258f74cb6e6cd24ea55681ed6ca
javigoo <javierroiggregorio@gmail.com>
2020-08-20 14:47:02 +0200

    Add login

--- Python/VRM API/dia-o-noche.py
+++ Python/VRM API/dia-o-noche.py
@@ -1,58 +1,72 @@
 #!/bin/env python3
 
+import os
 import sys
+import getpass
 import requests
   
 # api-endpoint 
 URL = "https://vrmapi.victronenergy.com/v2/"
 
 def login():
+    if 'login.dat' not in os.listdir("."):
+        username = str(input("Username: "))
+        password = str(getpass.getpass())
+        with open('login.dat', 'w') as f: 
+            f.write(username)
+            f.write(" ")
+            f.write(password) 
 
+def getUsername():
+    with open('login.dat', 'r') as f:
+        return f.readline().split()[0]
+
+def getPassword():
+    with open('login.dat', 'r') as f:
+        return f.readline().split()[1]
+
+def getLoginInfo():
     # Parameters to be sent to the API 
     url_login = URL + "auth/login"
-    data_login = '{"username": "john@example.com","password": "secret-passw0rd"}'
+    data_login = '{"username": "'+getUsername()+'","password": "'+getPassword()+'"}'
     
-    # sending request and saving the response as response object 
+    # Sending request and saving the response as response object 
     r = requests.post(url = url_login, data=data_login) 
-    data = r.json()
 
+    data = r.json()
     if len(data) > 2:
         sys.stderr.write('ERROR: '+data['errors']+'\n')
+        if data['errors'] == "Invalid credentials":
+            os.remove("login.dat")
         exit(-1)
         
-    # extracting data in json format 
     return data
 
 def getToken(data):
-
     return data['token']
 
 def getIdUser(data):
-
     return data['idUser']
 
-def get_installation_info(idUser, token):
-
+def getInstallationInfo(idUser, token):
     # Parameters to be sent to the API
     url_info = URL + "users/"+str(idUser)+"/installations"
     params_info = {'extended':1}
     headers_info = {'X-Authorization': "Bearer "+ str(token)}
 
-    # sending request and saving the response as response object
+    # Sending request and saving the response as response object
     r = requests.get(url = url_info, params=params_info, headers=headers_info) 
     
-    # extracting data in json format 
     return r.json() 
 
-def get_solar_yield(data):
-
+def getSolarYield(data):
     return data['records'][0]['extended'][12]['formattedValue']
   
 if __name__ == "__main__":
-
-    data_login = login()
-    data_installation = get_installation_info(getIdUser(data_login), getToken(data_login))
-    produccion_solar = get_solar_yield(data_installation)
+    login()
+    data_login = getLoginInfo()
+    data_installation = getInstallationInfo(getIdUser(data_login), getToken(data_login))
+    produccion_solar = getSolarYield(data_installation)
 
     if produccion_solar == "0 W":
         print("Es de noche")
